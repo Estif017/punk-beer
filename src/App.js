7ed605cc6ccc179ef33b2ca67c5605ac43dc23f3
Estif017 <estifoteklearegay@gmail.com>
2020-09-28 13:12:14 -0700

    adding the navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import './App.css';
+import Fettching from './Components/fettching';
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			data: {},
-		};
-	}
-	componentDidMount() {
-		const urlBase = 'https://api.punkapi.com/v2/beers?page=1';
-		fetch(urlBase)
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data);
-			});
-	}
-	render() {
-		return <div className='App'></div>;
-	}
-}
+const App = () => {
+	return (
+		<div className='container'>
+			<Fettching />
+		</div>
+	);
+};
 
 export default App;
